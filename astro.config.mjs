670d0c73d97ab04eb638cdf39700079059e2086c
Kenn <xramos@centro.edu.mx>
2025-06-04 00:11:03 -0600

    fix: configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://evryingreth.github.io/RETH",
-  integrations: [],
+  site: "https://kennxr.github.io/ALT",
+  base: "/ALT",
   output: 'static',
   build: {
     assets: '_assets'
